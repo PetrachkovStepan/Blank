a041ed7cccdb58b0ed2f71d5b67f9cc4b21c0d51
PetrachkovStepan <158708945+PetrachkovStepan@users.noreply.github.com>
2024-11-08 09:18:03 +0300

    feat: added constants fo info in lists

--- src/pages/OurServices/OurServices.tsx
+++ src/pages/OurServices/OurServices.tsx
@@ -1,5 +1,6 @@
 import AdvantagesList from "../../Components/Lists/AdvantagesList/AdvantagesList";
 import LinkList from "../../Components/Lists/LinksList/LinkList";
+import { ADVANTAGES_LIST_INFO, LINK_LIST_INFO } from "../../constants/listInfo";
 import "./styled.css";
 
 function OurServices() {
@@ -7,43 +8,9 @@ function OurServices() {
     <div className="ourServicesContainer">
       <span>OUR SERVICES</span>
       <div className="listsContainer">
-        <LinkList
-          items={[
-            "General PR Strategy (Strategy)",
-            "Media Relations (Media)",
-            "Pitch Deck Creation (Pitch)",
-            "Content Creation (Content)",
-            "Investor Relations Support(Investors)",
-          ]}
-        />
+        <LinkList items={LINK_LIST_INFO} />
         <AdvantagesList
-          items={[
-            {
-              head: `"Your roadmap to PR success."`,
-
-              text: `We analyze your communication and create a comprehensive plan with clear objectives, audiences, and action steps.`,
-            },
-            {
-              head: `"Get your story heard by the right voices."`,
-
-              text: `We build strong media connections and craft newsworthy content to maximize your visibility.`,
-            },
-            {
-              head: `"Investor-ready decks that win attention."`,
-
-              text: `Compelling presentations that tell your story and capture investor interest.`,
-            },
-            {
-              head: `"Content that keeps your audience engaged."`,
-
-              text: `Social media, blog, and newsletter management that resonates with your audience.`,
-            },
-            {
-              head: `"Position your business to attract investors."`,
-
-              text: `Visibility on investor platforms, pitches, and impactful meetings.`,
-            },
-          ]}
+          items={ADVANTAGES_LIST_INFO}
         />
       </div>
     </div>
